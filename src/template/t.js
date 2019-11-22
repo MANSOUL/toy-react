2fb0140e0a41c2c64ec0587691a8cccb3800e5d7
MANSOUL <xiaohu12347@gmail.com>
2019-11-22 11:53:49 +0800

    feat: parse attributes

--- src/template/t.js
+++ src/template/t.js
@@ -4,18 +4,23 @@ import createElement from '../createElement'
 const regTagStart = /^<(\w+)\s*([^>]*)\s*>/
 const regTagClose = /^<\/(\w+)\s*>/
 const regTagValue = /^[^<]*/
+const regTagAttr = /([\w-]+)="([^"]+)"/g
 const TAG_START = 'TAG_START'
 const TAG_CLOSE = 'TAG_CLOSE'
 const TAG_VALUE = 'TAG_VALUE'
+const TAG_ATTR_NAME = 'TAG_ATTR_NAME'
+const TAG_ATTR_VALUE = 'TAG_ATTR_VALUE'
 
 const parseToken = template => {
   let type = ''
   let matches = null
   let value = null
+  let attrs = ''
 
   if ((matches = template.match(regTagStart))) {
     type = TAG_START
     value = matches[1]
+    attrs = matches[2]
   } else if ((matches = template.match(regTagClose))) {
     type = TAG_CLOSE
     value = matches[1]
@@ -29,19 +34,41 @@ const parseToken = template => {
   return {
     type,
     value,
-    subIndex: matches.index + matches[0].length
+    subIndex: matches.index + matches[0].length,
+    attrs
   }
 }
 
+function parseAttr (attr) {
+  let m = null
+  const attrs = []
+
+  while ((m = regTagAttr.exec(attr))) {
+    attrs.push({
+      type: TAG_ATTR_NAME,
+      value: m[1]
+    })
+    attrs.push({
+      type: TAG_ATTR_VALUE,
+      value: m[2]
+    })
+  }
+  return attrs
+}
+
 const parseLexical = template => {
-  const tokens = []
+  let tokens = []
   let temp = trim(template)
   while (temp.length) {
     temp = trim(temp)
     const token = parseToken(temp)
     temp = temp.substring(token.subIndex)
     tokens.push(token)
+    if (token.type === TAG_START) {
+      tokens = tokens.concat(parseAttr(token.attrs))
+    }
   }
+  console.log(tokens)
   return tokens
 }
 
@@ -49,6 +76,7 @@ const parseSyntax = tokens => {
   let index = 0
   let nextToken = tokens[index]
   let current = null
+  let attrName = ''
 
   const tree = current = {
     type: 'root',
@@ -64,6 +92,7 @@ const parseSyntax = tokens => {
       const tempRoot = {
         type: nextToken.value,
         children: [],
+        props: {},
         parent: current
       }
       current.children.push(tempRoot)
@@ -72,6 +101,10 @@ const parseSyntax = tokens => {
       const tempRoot = current
       current = tempRoot.parent
       delete tempRoot.parent
+    } else if (nextToken.type === TAG_ATTR_NAME) {
+      attrName = nextToken.value
+    } else if (nextToken.type === TAG_ATTR_VALUE) {
+      current.props[attrName] = nextToken.value
     } else if (nextToken.type === TAG_VALUE) {
       current.children.push(nextToken.value)
     }
@@ -84,7 +117,7 @@ const parseSyntax = tokens => {
 
 const convertToElements = tree => {
   const children = tree.children ? tree.children.map(convertToElements) : []
-  return typeof tree === 'object' ? createElement(tree.type, null, ...children) : tree
+  return typeof tree === 'object' ? createElement(tree.type, tree.props, ...children) : tree
 }
 
 export const t = template => convertToElements(parseSyntax(parseLexical(template)))
